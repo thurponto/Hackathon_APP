2bdcc9c5c782fe3b1ac1ad4ff6727af472695100
pedro <pedrohenriqueluz2004@gmail.com>
2025-06-25 15:14:14 -0300

    sem o login

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'utils/app_theme.dart';
-import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/aluno_list_screen.dart';
 import 'screens/gabarito_screen.dart';
 import 'screens/selecionar_ra_screen.dart';
 
-
-Future<bool> checkLogin() async {
-  final prefs = await SharedPreferences.getInstance();
-  return prefs.getBool('isLoggedIn') ?? false;
-}
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  bool isLoggedIn = await checkLogin();
-
-  runApp(CorrecaoGabaritoApp(isLoggedIn: isLoggedIn));
+void main() {
+  runApp(CorrecaoGabaritoApp());
 }
 
 class CorrecaoGabaritoApp extends StatelessWidget {
-  final bool isLoggedIn;
-  CorrecaoGabaritoApp({required this.isLoggedIn});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Correção de Gabarito',
       theme: AppTheme.theme,
-      initialRoute: isLoggedIn ? '/home' : '/',
+      initialRoute: '/home',
       routes: {
-        '/': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
         '/alunos': (context) => AlunoListScreen(),
         '/gabarito': (context) => GabaritoScreen(),
@@ -41,4 +25,3 @@ class CorrecaoGabaritoApp extends StatelessWidget {
     );
   }
 }
-
